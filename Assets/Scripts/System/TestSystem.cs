9e0d9ef0ed4027ae5e3720270cf9cde8352dfc5f
Christian Hirschi <christian.hirschi@hotmail.com>
2020-05-31 18:26:30 +0200

    project setup

---
+++ Assets/Scripts/System/TestSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class TestSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float dT = Time.DeltaTime;
+        Entities
+            .WithAll<TestTag>().ForEach(
+                (ref Translation translation, ref Rotation rotation) =>
+                {
+                    translation.Value.z += 5 * dT;
+                    rotation.Value = math.mul(rotation.Value, quaternion.RotateZ(10 * dT));
+                }
+            )
+            .Schedule();
+    }
+}
