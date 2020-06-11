e4f46a6e79e6fcc88057d8f2ab78eb67d1860a58
Ultimatonium <ultimatonium@users.noreply.github.com>
2020-06-12 00:00:27 +0200

    pathfinding -buggy working

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -26,13 +26,13 @@ public class Pathfinding : MonoBehaviour
         NavMeshTriangulation navMeshTriangulation = NavMesh.CalculateTriangulation();
         GetComponent<MeshFilter>().mesh.vertices = navMeshTriangulation.vertices;
         GetComponent<MeshFilter>().mesh.triangles = navMeshTriangulation.indices;
-        navMeshVertices = new NativeArray<float3>(navMeshTriangulation.vertices.Length, Allocator.Temp);
+        navMeshVertices = new NativeArray<float3>(navMeshTriangulation.vertices.Length, Allocator.Persistent);
         for (int i = 0; i < navMeshTriangulation.vertices.Length; i++)
         {
             navMeshVertices[i] = navMeshTriangulation.vertices[i];
         }
 
-        navMeshIndices = new NativeArray<int>(navMeshTriangulation.indices.Length, Allocator.Temp);
+        navMeshIndices = new NativeArray<int>(navMeshTriangulation.indices.Length, Allocator.Persistent);
         for (int i = 0; i < navMeshTriangulation.indices.Length; i++)
         {
             navMeshIndices[i] = navMeshTriangulation.indices[i];
@@ -73,12 +73,23 @@ public class Pathfinding : MonoBehaviour
 
         }
 
+           
+
+    }
+
+    private bool first = true;
+    private void Update()
+    {
+        if (first)
+        {
         GetPath(start.transform.position, end.transform.position);
+            first = false;
+        }
     }
 
     private void paintVertexFromTriangleIndex(int index)
     {
-        TextMeshPro textMeshObject = Instantiate(textMeshPrefab, navMeshVertices[navMeshIndices[index]], textMeshPrefab.transform.rotation).GetComponent<TextMeshPro>();
+        //TextMeshPro textMeshObject = Instantiate(textMeshPrefab, navMeshVertices[navMeshIndices[index]], textMeshPrefab.transform.rotation).GetComponent<TextMeshPro>();
         /*
         foreach (TextMeshPro item in FindObjectsOfType<TextMeshPro>())
         {
@@ -88,8 +99,32 @@ public class Pathfinding : MonoBehaviour
             }
         }
         */
-        textMeshObject.text = navMeshIndices[index] + ":" + navMeshVertices[navMeshIndices[index]].ToString();
+        //textMeshObject.text = "index triangle: " + index + "|" + "index vertex: " + navMeshIndices[index] + ":" + navMeshVertices[navMeshIndices[index]].ToString();
+        //textMeshObject.name = textMeshObject.text;
+        writeTextMesh(navMeshVertices[navMeshIndices[index]], "index triangle: " + index + "|" + "index vertex: " + navMeshIndices[index] + ":" + navMeshVertices[navMeshIndices[index]].ToString());
+    }
+
+    private void writeTextMesh(float3 position, string text)
+    {
+        writeTextMesh(position, text, textMeshPrefab.GetComponent<TextMeshPro>().fontSize, textMeshPrefab.GetComponent<TextMeshPro>().color);
+    }
+
+    private void writeTextMesh(float3 position, string text, float fontSize, Color color)
+    {
+        TextMeshPro textMeshObject = Instantiate(textMeshPrefab, position, textMeshPrefab.transform.rotation).GetComponent<TextMeshPro>();
+        /*
+        foreach (TextMeshPro item in FindObjectsOfType<TextMeshPro>())
+        {
+            if (item.transform.position == textMeshObject.transform.position)
+            {
+                textMeshObject.transform.position += new Vector3(0, 0, -1);
+            }
+        }
+        */
+        textMeshObject.text = text;
         textMeshObject.name = textMeshObject.text;
+        textMeshObject.fontSize = fontSize;
+        textMeshObject.color = color;
     }
 
     public NativeArray<PathNode> GetPath(float3 start, float3 end)
@@ -107,23 +142,41 @@ public class Pathfinding : MonoBehaviour
                 hCost = CalculateDistanceCost(start, end),
                 prevIndex = -1,
             };
+        }
+
+        for (int i = 0; i < pathNodes.Length; i++)
+        {
             NativeArray<int> vertexNeighbours = GetIndicesOfVertexNeighbours(i);
             for (int ii = 0; ii < vertexNeighbours.Length; ii++)
             {
                 neighbourIndices.Add(i, vertexNeighbours[ii]);
-                if (i == 463)
+                /*
+                if (i == 459)
                 {
-                //Debug.Log("add " + i + " " + vertexNeighbours[ii]);
-                }
+                Debug.Log("add " + i + " " + vertexNeighbours[ii] + " of " + vertexNeighbours.Length);
+                }*/
             }
         }
+
+
+                //Debug.Log("230" + pathNodes[230].position);
+        //Debug.Log("320" + pathNodes[320].position);
+        //if((pathNodes[230].position).Equals((pathNodes[320].position)))
+        /*
+        if(EqualFloat3(pathNodes[230].position, pathNodes[320].position,0.1f))
+        {
+            Debug.Log("same");
+        } else
+        {
+            Debug.Log("not same");
+        }*/
 
         NativeList<int> openList = new NativeList<int>(Allocator.Temp);
         NativeList<int> closedList = new NativeList<int>(Allocator.Temp);
 
         int startNodeIndex = CalculateIndex(start);
         int endNodeIndex = CalculateIndex(end);
-        Debug.Log(startNodeIndex + " " + endNodeIndex);
+        Debug.Log("start/end:" + startNodeIndex + " " + endNodeIndex);
         Debug.DrawLine(pathNodes[startNodeIndex].position, pathNodes[endNodeIndex].position, Color.blue, 100);
 
         //PathNode startNode = pathNodes[CalculateIndex(startNodeIndex)];
@@ -133,10 +186,13 @@ public class Pathfinding : MonoBehaviour
 
         openList.Add(startNode.index);
 
+        int order = -1;
         while (openList.Length > 0)
         {
+            order++;
             int currentNodeIndex = GetLowestCostFNodeIndex(openList, pathNodes);
-            Debug.Log(currentNodeIndex);
+            writeTextMesh(pathNodes[currentNodeIndex].position, "Order:" + order, 10, Color.black);
+            //Debug.Log(currentNodeIndex);
 
             if (currentNodeIndex == endNodeIndex)
             {
@@ -175,27 +231,31 @@ public class Pathfinding : MonoBehaviour
                     Debug.Log("found " + neighbourIndex);
                 }
                 PathNode neighbourNode = pathNodes[neighbourIndex];
-                /*
                 if (closedList.Contains(neighbourNode.index))
                 {
                     // Already searched this node
                     continue;
-                }*/
+                }
+                if (neighbourNode.position.y > 1)
+                {
+                    //ignore jump
+                    continue;
+                }
                 if (!closedList.Contains(neighbourNode.index))
                 {
 
-                    //float tentativeGCost = pathNodes[currentNodeIndex].gCost + CalculateDistanceCost(pathNodes[currentNodeIndex].position, neighbourNode.position);
-                    //if (tentativeGCost < neighbourNode.gCost)
+                    float tentativeGCost = pathNodes[currentNodeIndex].gCost + CalculateDistanceCost(pathNodes[currentNodeIndex].position, neighbourNode.position);
+                    if (tentativeGCost < neighbourNode.gCost)
                     {
                         neighbourNode.prevIndex = currentNodeIndex;
-                        //neighbourNode.gCost = tentativeGCost;
+                        neighbourNode.gCost = tentativeGCost;
                         pathNodes[neighbourNode.index] = neighbourNode;
 
                         if (!openList.Contains(neighbourNode.index))
                         {
                             openList.Add(neighbourNode.index);
                             Debug.DrawLine(pathNodes[currentNodeIndex].position, neighbourNode.position, Color.red, 10);
-                            Debug.Log(currentNodeIndex + " " + neighbourNode.index);
+                            //Debug.Log(currentNodeIndex + " " + neighbourNode.index);
                         }
                     }
                 }
@@ -214,6 +274,7 @@ public class Pathfinding : MonoBehaviour
         {
             // Found a path
             Debug.Log("found a path");
+            drawPath(pathNodes, endNode);
             //NativeList<int2> path = CalculatePath(pathNodeArray, endNode);
             /*
             foreach (int2 pathPosition in path) {
@@ -229,6 +290,35 @@ public class Pathfinding : MonoBehaviour
         return new NativeArray<PathNode>();
     }
 
+    private void drawPath(NativeArray<PathNode> pathNodes, PathNode endNode)
+    {
+        if (endNode.prevIndex == -1)
+        {
+            return;
+        } else
+        {
+            PathNode node = endNode;
+            while(node.prevIndex != -1)
+            {
+                PathNode prevNode = pathNodes[node.prevIndex];
+                Debug.DrawLine(node.position, prevNode.position, Color.green, 500);
+                node = prevNode;
+            }    
+        }
+    }
+
+    private bool EqualFloat3(float3 a, float3 b, float tolerance)
+    {
+        return math.abs(a.x - b.x) < tolerance 
+            && math.abs(a.y - b.y) < tolerance 
+            && math.abs(a.z - b.z) < tolerance;
+    }
+
+    private bool EqualFloat(float a, float b, float tolerance)
+    {
+        return math.abs(a - b) < tolerance;
+    }
+
     private NativeArray<int> GetIndicesOfVertexNeighbours(int vertexIndex)
     {
         //NativeList<int> neighboursIndices = new NativeList<int>(Allocator.Temp);
@@ -238,48 +328,76 @@ public class Pathfinding : MonoBehaviour
         int neighbourIndex1 = 0;
         int neighbourIndex2 = 0;
 
+        int outi = 0;
         for (int i = 0; i < navMeshIndices.Length; i++)
         {
-            if(math.abs(pathNodes[navMeshIndices[i]].position).Equals(math.abs(pathNodes[vertexIndex].position)))
+            /*
+            if (vertexIndex == 459 && navMeshIndices[i] == 552 || vertexIndex == 552)
+            {
+                Debug.Log(vertexIndex + "(" + pathNodes[vertexIndex].position + ")" + " pre mod " + i + " " + navMeshIndices[i] + "(" + pathNodes[navMeshIndices[i]].position + ")");
+            }*/
+            if (EqualFloat3(pathNodes[navMeshIndices[i]].position, pathNodes[vertexIndex].position, 0.1f))
             {
                 int remainder = navMeshIndices[i] % 3;
                 switch (remainder)
                 {
                     case 0:
-                        neighbourIndex1 = navMeshIndices[i + 1];
-                        neighbourIndex2 = navMeshIndices[i + 2];
+                        neighbourIndex1 = TryGetNavMeshIndices(i + 1);
+                        neighbourIndex2 = TryGetNavMeshIndices(i + 2);
                         break;
                     case 1:
-                        neighbourIndex1 = navMeshIndices[i - 1];
-                        neighbourIndex2 = navMeshIndices[i + 1];
+                        neighbourIndex1 = TryGetNavMeshIndices(i - 1);
+                        neighbourIndex2 = TryGetNavMeshIndices(i + 1);
                         break ;
                     case 2:
-                        neighbourIndex1 = navMeshIndices[i - 2];
-                        neighbourIndex2 = navMeshIndices[i - 1];
+                        neighbourIndex1 = TryGetNavMeshIndices(i - 2);
+                        neighbourIndex2 = TryGetNavMeshIndices(i - 1);
                         break;
                     default:
                         Debug.LogError("Impossible remainder " + remainder);
                         break;
                 }
                 //Debug.Log(i + " " + neighbourIndex1 + " " + neighbourIndex2);
+                //neighboursIndices.Add(neighbourIndex1);
+                //neighboursIndices.Add(neighbourIndex2);
                 neighboursIndices.TryAdd(neighbourIndex1, navMeshVertices[neighbourIndex1]);
-                neighboursIndices.TryAdd(neighbourIndex2, navMeshVertices[neighbourIndex2]);
+                neighboursIndices.TryAdd(neighbourIndex2, navMeshVertices[neighbourIndex2]); 
                 /*
-                if (vertexIndex == 0)
+                if (vertexIndex == 459)
                 {
-                    Debug.Log("found 0 for " + i);
+                    Debug.Log("found for " + i);
                     Debug.Log(neighbourIndex1 + " " + neighbourIndex2);
                 }*/
             }
+            outi = i;
         }
+        /*
+        if (vertexIndex == 459 || vertexIndex == 552)
+        {
+            Debug.Log("outi of :" + vertexIndex + " " + navMeshIndices.Length + " " + outi);
+        }*/
+        /*
         for (int i = 0; i < neighboursIndices.GetKeyArray(Allocator.Temp).Length; i++)
         {
-            if (vertexIndex == 0)
+            if (vertexIndex == 459)
             {
             Debug.Log("search " + vertexIndex + " " + neighboursIndices.GetKeyArray(Allocator.Temp)[i]);
             }
-        }
+        }*/
         return neighboursIndices.GetKeyArray(Allocator.Temp);
+        //return neighboursIndices.AsArray();
+    }
+
+    private int TryGetNavMeshIndices(int index)
+    {
+        if (index < navMeshIndices.Length)
+        {
+            return navMeshIndices[index];
+        } else
+        {
+            Debug.LogWarning("out of range" + index + "/" + navMeshIndices.Length);
+            return navMeshIndices[0];
+        }
     }
 
     private int CalculateIndex(float3 pos)
