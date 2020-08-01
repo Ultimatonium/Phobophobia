f3dba47e2295b1bb6b7df35b57b0335f87f15d45
Ultimatonium <ultimatonium@users.noreply.github.com>
2020-08-01 19:36:05 +0200

    add load quotes

---
+++ Assets/QuoteReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class QuoteReader : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset quoteFile;
+    [SerializeField]
+    private TextMeshProUGUI quotePresenter;
+
+    private void Start()
+    {
+        string[] quotes = quoteFile.text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+        quotePresenter.text = quotes[UnityEngine.Random.Range(0, quotes.Length)];
+    }
+}
