85ef4a0ca89ac74b57e14be937566ac6fc62d2cc
Daniel Dubs <danubsubs@hotmail.com>
2020-08-22 17:52:48 +0200

    FMOD Extensions and Optimizations
    - Die Lautstärkeregler in den beiden Optionsmenüs funktionieren jetzt!
    - Studio-API-Fehlerüberprüfung hinzugefügt - einen kleinen Bug behoben.
    - Der Sound zur Warnung bei niedrigen Lebenspunkten ertönt jetzt früher, nämlich schon bei weniger als einem Fünftel des Maximums.
    - Der Warnungs- und der Respawn-Sound sind nun etwas lauter.

---
+++ Assets/Scripts/MonoBehaviour/Audio/FMOD/FMODErrorHandling.cs
@@ -0,0 +1,9 @@
+﻿//Actually, this should really be done by FMOD for Unity!
+public class FMODErrorHandling : UnityEngine.MonoBehaviour
+{
+  public static void CheckRESULT(FMOD.RESULT returnValue)
+  {
+    if(returnValue != FMOD.RESULT.OK)
+      UnityEngine.Debug.LogWarning("FMOD-Error: " + FMOD.Error.String(returnValue));
+  }
+}
---
+++ Assets/Scripts/MonoBehaviour/Audio/FMOD/VolumeChanger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class VolumeChanger : MonoBehaviour
+{
+  [SerializeField] private UnityEngine.UI.Slider volumeSlider = null;
+  [Header("FMOD")]
+  [FMODUnity.EventRef][SerializeField] private string masterBusPath = "bus:/Master";
+  [FMODUnity.EventRef][SerializeField] private string pausePatterBusPath = "bus:/PausePatter";
+  [Space(10)]
+  [SerializeField] private float initialVolume = 0.5f;
+
+  private FMOD.Studio.Bus masterBus;
+
+  private void Start()
+  {
+    masterBus = FMODUnity.RuntimeManager.GetBus(masterBusPath);
+    volumeSlider.value = initialVolume;
+  }
+
+  public void ChangeVolume(float newValue)
+  {
+    if(!masterBus.isValid())
+      return;
+
+    CheckRESULT(masterBus.setVolume(newValue));
+    CheckRESULT(FMODUnity.RuntimeManager.GetBus(pausePatterBusPath).setVolume(newValue));
+
+    //Debug.Log("New volume-value: " + newValue);
+  }
+
+  private void CheckRESULT(FMOD.RESULT returnValue)
+  {
+    if(returnValue != FMOD.RESULT.OK)
+      Debug.LogWarning("FMOD-Error: " + FMOD.Error.String(returnValue));
+  }
+}
